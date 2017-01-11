a979e7b6d35fc01c0e27a1aa28127740d2347fb1
kenta-suzuki <kenta.suzuki@befool.co.jp>
2017-01-11 18:51:37 +0900

    APIとclientに分けた

---
+++ client/ConectionToAPI/Assets/Model/Data/BBS.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BBS : BaseData
+{
+	public string Subject;
+	public string Text;
+	public string Email;
+	public string Password;
+	public string FileName;
+	public int ParentBbsId;
+	public bool IsDeleted;
+}
---
+++ client/ConectionToAPI/Assets/Scripts/APIConnectionTest.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class APIConnectionTest : MonoBehaviour
+{
+	const string URL = "http://192.168.33.19:8000/bbs/";
+	WWW www;
+
+	public void SendRequest()
+	{
+		
+	}
+
+	void Start()
+	{
+		StartCoroutine(WaitForResponse());
+	}
+
+	IEnumerator WaitForResponse()
+	{
+		var requestURL = URL;
+		www = new WWW(requestURL);
+
+		yield return www;
+
+		if (!string.IsNullOrEmpty(www.error))
+		{
+			Debug.LogError(string.Format("Fail Whale!\n{0}", www.error));
+			yield break;
+		}
+		string json = www.text;
+		JSONObject jsonObj = new JSONObject(json);
+
+		foreach (var data in jsonObj.list)
+		{
+			Debug.Log(data.GetField("subject"));
+		}
+	}
+}
